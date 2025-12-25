e64713d1b837aaa3822c8bb89f281e22fa5dd415
yelhaoua <yacobelhaouarii@gmail.com>
2025-12-25 12:45:32 +0100

    return

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"net/http"
+
+	"groupie/handlers"
+)
+
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/statics/", handlers.HandleForbiden)
+	mux.HandleFunc("/", handlers.HandleHome)
+	mux.HandleFunc("/artist/{id}", handlers.HandleInfo)
+
+	http.ListenAndServe(":8080", mux)
 }
